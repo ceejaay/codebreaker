7fe6512e4670430be876b0824b06b452f1db19f2
Chad Jemmett <chad.jemmett@gmail.com>
2014-01-13 08:28:58 -0700

    stopped on pg 45 wrote in first rspec tests 5.1

---
+++ spec/codebreaker/game_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+module Codebreaker
+  describe Game do
+    desecribe "#start" do
+      it "sends a welcome message"
+      it "prompts for the first guest"
+    end
+  end
+end
